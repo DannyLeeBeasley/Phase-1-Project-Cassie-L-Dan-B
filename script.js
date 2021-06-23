d37f301e266072c60a89d0c694f500e87c647ca9
Dan Beasley <dannyleebeasley@gmail.com>
2021-06-23 16:08:03 -0500

    All 3 Core Deliverables Working!

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const visitedForm = document.querySelector('#visted-form')
-const wantToForm = document.querySelector('#want-to-form')
-const visitedContainer = document.querySelector('#visted-container')
-const wantToContainer = document.querySelector('#want-to-container')
-const visitedInputField = document.querySelector('#visited-input-field')
-const wantToInputField = document.querySelector('#want-to-input-field')
-const getInfoForm = document.querySelector()
-const getInfoInput =
-const languagesHolder = 
+const getInfoForm = document.querySelector('#country-info-form')
+const getInfoInput = document.querySelector('#country-info-input')
 
+const nameHolder = document.querySelector('#name-container')
+const flagHolder = document.querySelector('#flag-container')
+const languagesHolder = document.querySelector('#language-container')
+const currencyHolder = document.querySelector('#currency-container')
+const borderingCountriesHolder = document.querySelector('#bordering-countries-container')
+
+
+const visitedForm = document.querySelector('#visted-form')
+const visitedInputField = document.querySelector('#visited-input-field')
+const visitedContainer = document.querySelector('#visted-container')
+
+const wantToForm = document.querySelector('#want-to-form')
+const wantToInputField = document.querySelector('#want-to-input-field')
+const wantToContainer = document.querySelector('#want-to-container')
+
+
+
+getInfoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    countryName = getInfoInput.value
+    getOneCountry(countryName)
+    e.target.reset()
+})
 
 visitedForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -25,29 +41,45 @@ wantToForm.addEventListener('submit', (e) => {
     e.target.reset()
 })
 
-
-
-function getAllCountries(){
-    fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(arrayOfCountries => console.log(arrayOfCountries))
-}
-
 function getOneCountry(countryName){
-    fetch(`https://restcountries.eu/rest/v2/${countryName}`)
+    fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
     .then(res => res.json())
-    .then(arrayOfCountries => arrayOfCountries.forEach(
-        countryObj => {}
-    ))
+    .then(countryObj => renderCountryInfo(countryObj))
 }
 
-getOneCountry()
+function renderCountryInfo(countryObj){
+    nameHolder.innerHTML = ''
+    flagHolder.innerHTML = ''
+    languagesHolder.innerHTML = ''
+    currencyHolder.innerHTML = ''
+    borderingCountriesHolder.innerHTML = ''
 
-// function filterByLanguage(countryObj){
-//     if (countryObj.languages.name === "English"){
-//         const languageFilterContainer = document.querySelector('#language-filter-container')
-//         languageFilterContainer.append(countryObj.name)
-//     }
+    let countryNameH4 = document.createElement('h4')
+    let flagImage = document.createElement('img')
+    let languagesList = document.createElement('ul')
+    let currencyList = document.createElement('ul')
+
+    countryNameH4.textContent = countryObj[0].name
+    flagImage.src = countryObj[0].flag 
+    languagesList.textContent = countryObj[0].languages[0].name
+    currencyList.textContent = countryObj[0].currencies[0].name
+
+    nameHolder.append(countryNameH4)
+    flagHolder.append(flagImage)
+    languagesHolder.append(languagesList)
+    currencyHolder.append(currencyList)
+    countryObj[0].borders.forEach(element => {
+        let borderingCountryLi = document.createElement('li')
+        borderingCountryLi.textContent = element
+        borderingCountriesHolder.append(borderingCountryLi)
+    }
+    )
+}
+
+// function getAllCountries(){
+//     fetch('https://restcountries.eu/rest/v2/all')
+//     .then(res => res.json())
+//     .then(arrayOfCountries => console.log(arrayOfCountries))
 // }
 
 // getAllCountries()
